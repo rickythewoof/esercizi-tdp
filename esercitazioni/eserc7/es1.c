6e5e0b431b2db5089d82350ce0cf3d5b14211d9f
Ricky <riccardo.zucchelli@gmail.com>
2022-04-12 11:16:33 +0200

    added esercitazione bonus

--- esercitazioni/eserc7/es1.c
+++ esercitazioni/eserc7/es1.c
@@ -12,7 +12,7 @@ struct ElemSCL *next;
 typedef struct ElemSCL TipoNodoSCL;
 typedef TipoNodoSCL * TipoSCL;
 
-void mkSCL(TipoSCL* scl, int n, TipoInfoSCL e);
+void scl_make(TipoSCL* scl, int n, TipoInfoSCL e);
 void scl_print(TipoSCL scl);
 
 int scl_len(TipoSCL scl);
@@ -24,24 +24,24 @@ TipoSCL scl_positives(TipoSCL scl);
 
 int main(){
     TipoSCL scl1;
-    mkSCL(&scl1, 5, -2);
+    scl_make(&scl1, 5, -2);
     scl_print(scl1);
     printf("LEN = %d\n", scl_len(scl1));
     printf("SUM = %f\n",scl_sum(scl1));
     printf("MED = %f\n",scl_media(scl1));
-    TipoSCL scl2; mkSCL(&scl2, 5, 10);
+    TipoSCL scl2; scl_make(&scl2, 5, 10);
     printf("PROD = %f\n", scl_dot(scl1,scl2));
     scl_duplicate_pos(scl2, 3); scl_print(scl2);
     TipoSCL scl3 = scl_positives(scl1); scl_print(scl3);
     return 0;
 }
 
-void mkSCL (TipoSCL* scl, int n, TipoInfoSCL e){
+void scl_make (TipoSCL* scl, int n, TipoInfoSCL e){
     if (n == 0) *scl = NULL;
     else {
         *scl = (TipoNodoSCL*)malloc(sizeof(TipoNodoSCL));
         (*scl)->info = e;
-        mkSCL(&((*scl)->next), n-1, e+2);
+        scl_make(&((*scl)->next), n-1, e+2);
     }
 }
 
