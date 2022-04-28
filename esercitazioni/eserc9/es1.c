b434c1d7653be2ea836121a52843c02b32a764a2
Ricky <riccardo.zucchelli@gmail.com>
2022-04-28 22:49:24 +0200

    Finito esercizio9

---
+++ esercitazioni/eserc9/es1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+TipoLista init(T *vec, int len);
+TipoLista doubledCopy(TipoLista list);
+TipoLista multipleSublist( TipoLista list, unsigned int m );
+TipoLista subList(TipoLista list, int start, int end);
+TipoLista interleave(TipoLista l1, TipoLista l2);
+
+int main(){
+  int array[] = {1,2,3,4,5};
+  TipoLista list1 = init(array, 5);
+  printf("LISTA =\t\t"); printlist(list1);
+  printf("LISTA_D =\t"); printlist(doubledCopy(list1));
+  printf("LISTA_MULT = \t"); printlist(multipleSublist(list1, 2));
+  printf("LISTA_SUB = \t"); printlist(subList(list1, 2, 5));
+  printf("LISTA_INT = \t"); printlist(interleave(list1, list1));
+  return 0;
+}
+
+TipoLista init(T *vec, int len){
+  if (len == 0) return listaVuota();
+  else return cons(*vec, init(vec+1, len-1));
+}
+
+TipoLista doubledCopy_aux(TipoLista list, int toRepeat){
+  if (estVuota(list)) return listaVuota();
+  T elem = car(list);
+  if (toRepeat == 1)
+    return cons(elem, doubledCopy_aux(list,0));
+  else
+    return cons(elem, doubledCopy_aux(cdr(list),1));
+}
+
+TipoLista doubledCopy(TipoLista list){
+  return doubledCopy_aux(list, 1);
+}
+
+TipoLista multipleSublist( TipoLista list, unsigned int m ){
+  if (estVuota(list)) return listaVuota();
+  T elem = car(list);
+  if (elem % m == 0) return cons(elem, multipleSublist(cdr(list), m));
+  else return multipleSublist(cdr(list), m);
+}
+
+TipoLista subList(TipoLista list, int start, int end){
+  if (estVuota(list)) return listaVuota();
+  T elem = car(list);
+  if (start <= elem && elem < end) return cons(elem, subList(cdr(list), start, end));
+  else return subList(cdr(list), start, end);
+}
+
+TipoLista interleave(TipoLista l1, TipoLista l2){
+  if (estVuota(l1)) return listaVuota();
+  else return cons(car(l1), interleave(l2, cdr(l1)));
+}
