48aef940e4ba943772ca8f29c3d4d65659896d23
Ricky <riccardo.zucchelli@gmail.com>
2022-05-16 10:57:25 +0200

    manca albero_lista e da fixare somma_sottoalbero

--- esercitazioni/eserc11/es1.c
+++ esercitazioni/eserc11/es1.c
@@ -26,16 +26,26 @@ TipoInfoAlbero max(int a, int b){
   else return b;
 }
 
+int len_list(TipoLista l){
+  if(estVuota(l))
+    return 0;
+  else
+    return 1 + len_list(cdr(l));
+}
+
 int main(){
-  TipoAlbero alb = creaAlbBin(1, creaAlbBin(2, creaAlbBin(4,NULL,NULL), NULL),
-                  creaAlbBin(3, creaAlbBin(5,NULL,NULL),
-                  creaAlbBin(6, NULL, creaAlbBin(7, NULL, NULL))));
+  TipoAlbero alb = creaAlbBin(1, creaAlbBin(2, creaAlbBin(4,NULL,NULL), creaAlbBin(5, NULL, NULL)),
+                  creaAlbBin(3, creaAlbBin(6,NULL,NULL),
+                  creaAlbBin(7, NULL, creaAlbBin(8, NULL, NULL))));
   printf("ALBERO=\t"), stampaParentetica(alb); printf("\n");
   printf("MAX=\t\t%d\n", trova_massimo(alb));
   printf("SOMMA_FOGLIE=\t%d\n", somma_foglie(alb));
   printf("CERCA_LIVELLO=\t%d\n", cerca_livello(alb, 4));
   printf("CONTA_DISPARI=\t%d\n", conta_dispari(alb));
   printf("SOMMA_SINGOLI\t%d\n", somma_singoli(alb));
+  printf("PERCORSO_LUNGO=\t"); printlist(percorso_lungo(alb)); printf("\n");
+  printf("SOMMA_SOTTOALBERO=\t"); stampaParentetica(somma_sottoalbero(alb)); printf("\n");
+  printf("SCAMBIA_FOGLIE=\t"); stampaParentetica(scambia_foglie(alb)); printf("\n");
 }
 
 TipoInfoAlbero trova_massimo(TipoAlbero a){
@@ -43,8 +53,7 @@ TipoInfoAlbero trova_massimo(TipoAlbero a){
   else if (estFoglia(a)) return radice(a);
   else{
     int m1 = max(trova_massimo(destro(a)), trova_massimo(sinistro(a)));
-    TipoInfoAlbero info = radice(a);
-    return max(info, m1);
+    return max(radice(a), m1);
   }
 }
 
@@ -60,10 +69,12 @@ int cerca_livello_aux(TipoAlbero a, TipoInfoAlbero v, int liv){
   if (estVuoto(a)) return -1;
   else if (radice(a) == v) return liv;
   else{
-    if (!estVuoto(sinistro(a)))
-      return cerca_livello_aux(sinistro(a), v, liv+1);
-    else if (!estVuoto(destro(a)))
-      return cerca_livello_aux(destro(a), v, liv+1);
+    int sx = cerca_livello_aux(sinistro(a), v, liv+1);
+    int dx = cerca_livello_aux(destro(a), v, liv+1);
+    if (sx != -1)
+      return sx;
+    else
+      return dx;
   }
 }
 
@@ -101,3 +112,50 @@ int somma_singoli(TipoAlbero a){
 //     if ()
 //   }
 // }
+
+TipoLista percorso_lungo(TipoAlbero a){
+  if (estVuoto(a))
+    return listaVuota();
+  else if (estFoglia(a))
+    return cons(radice(a), percorso_lungo(sinistro(a)));
+  else if (!estVuoto(sinistro(a))&&estVuoto(destro(a)))
+    return cons(radice(a), percorso_lungo(sinistro(a)));
+  else if (!estVuoto(destro(a))&&estVuoto(sinistro(a)))
+    return cons(radice(a), percorso_lungo(destro(a)));
+  else {
+    TipoLista dx = cons(radice(a),percorso_lungo(destro(a)));
+    TipoLista sx = cons(radice(a), percorso_lungo(sinistro(a)));
+    if(len_list(dx) > len_list(sx))
+      return dx;
+      else
+      return sx;
+  }
+}
+
+TipoAlbero somma_sottoalbero(TipoAlbero a){
+  if (estVuoto(a))
+    return albBinVuoto();
+  else if (estFoglia(a))
+    return creaAlbBin(0, albBinVuoto(), albBinVuoto());
+  else{
+    if (!estVuoto(sinistro(a))&&estVuoto(destro(a)))
+      return creaAlbBin(radice(somma_sottoalbero(sinistro(a))), sinistro(a), albBinVuoto());
+    else if (!estVuoto(destro(a))&&estVuoto(sinistro(a)))
+      return creaAlbBin(radice(somma_sottoalbero(destro(a))), albBinVuoto(), destro(a));
+    else
+      return creaAlbBin(radice(somma_sottoalbero(destro(a)))+ radice(somma_sottoalbero(sinistro(a))), albBinVuoto(), destro(a));
+  }
+}
+
+TipoAlbero scambia_foglie(TipoAlbero a){
+  if (estVuoto(a))
+    return albBinVuoto();
+  else if (estFoglia(sinistro(a)) && estFoglia(destro(a)))
+    return creaAlbBin(radice(a), destro(a), sinistro(a));
+  else if (!estVuoto(sinistro(a))&&estVuoto(destro(a)))
+    return creaAlbBin(radice(a), scambia_foglie(sinistro(a)), albBinVuoto());
+  else if (!estVuoto(destro(a))&&estVuoto(sinistro(a)))
+    return creaAlbBin(radice(a), albBinVuoto(), scambia_foglie(destro(a)));
+  else
+    return creaAlbBin(radice(a), scambia_foglie(sinistro(a)), scambia_foglie(destro(a)));
+}
